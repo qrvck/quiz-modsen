29386aa32eba2bc422c49875b68db58c051c574e
Uladzislau Kapitan <v.kapitan2010@gmail.com>
2025-06-08 21:03:42 +0300

    feat: добавил лоадер

---
+++ src/shared/UI/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import { getRandomNumber } from 'shared/lib/getRandomNumber';
+import styles from './Loader.module.scss';
+
+const VIEWS = ['view-1', 'view-2', 'view-3'];
+
+export function Loader() {
+  const viewStyleIndex = getRandomNumber(0, VIEWS.length);
+  const view = VIEWS[viewStyleIndex];
+  const viewStyle = styles[view];
+
+  return <div className={viewStyle} />;
+}
