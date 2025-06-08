005003615c4d6890620ecc4698d93e6082a5fd65
Uladzislau Kapitan <v.kapitan2010@gmail.com>
2025-06-09 01:49:41 +0300

    feat: добавил suspence

---
+++ src/widgets/Suspense/Suspense.tsx
@@ -0,0 +1,17 @@
+import React, { PropsWithChildren } from 'react';
+import { Loader } from 'shared/UI/Loader';
+import styles from './Suspense.module.scss';
+
+export function Suspense({ children }: PropsWithChildren) {
+  return (
+    <React.Suspense
+      fallback={
+        <div className={styles.wrapper}>
+          <Loader />
+        </div>
+      }
+    >
+      {children}
+    </React.Suspense>
+  );
+}
